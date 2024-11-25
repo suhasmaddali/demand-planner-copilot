99cc0b7065ca194ad18c38de6a45bb08b16d3ae4
suhasmaddali <suhas.maddali.chinnu@gmail.com>
2024-11-24 17:28:07 -0800

    added environment variables instead of st.secrets

--- app_demo_working.py
+++ app_demo_working.py
@@ -28,33 +28,56 @@ from src.utils import index_time_profile, convert_to_anaplan
 
 import streamlit as st
 
-# Getting access to credentials for loading data to database
-platform = st.secrets['DATABASE']['PLATFORM']
-aws_access_key_id = st.secrets['DATABASE']['AWS_ACCESS_KEY_ID']
-aws_secret_access_key = st.secrets['DATABASE']['AWS_SECRET_ACCESS_KEY']
-region_name = st.secrets['DATABASE']['REGION_NAME']
-memory_location = st.secrets['DATABASE']['BUCKET']
-number = st.secrets['DATABASE']['NUMBER']
+# # Getting access to credentials for loading data to database
+# platform = st.secrets['DATABASE']['PLATFORM']
+# aws_access_key_id = st.secrets['DATABASE']['AWS_ACCESS_KEY_ID']
+# aws_secret_access_key = st.secrets['DATABASE']['AWS_SECRET_ACCESS_KEY']
+# region_name = st.secrets['DATABASE']['REGION_NAME']
+# memory_location = st.secrets['DATABASE']['BUCKET']
+# number = st.secrets['DATABASE']['NUMBER']
 
-# Getting access to user credentials to validate login
-username_credentials = st.secrets['USER CREDENTIALS']['USERNAME']
-password_credentials = st.secrets['USER CREDENTIALS']['PASSWORD']
+# # Getting access to user credentials to validate login
+# username_credentials = st.secrets['USER CREDENTIALS']['USERNAME']
+# password_credentials = st.secrets['USER CREDENTIALS']['PASSWORD']
 
-base_url = st.secrets['USER CREDENTIALS']['BASE_URL']
-api_key = st.secrets['USER CREDENTIALS']['API_KEY']
+# base_url = st.secrets['USER CREDENTIALS']['BASE_URL']
+# api_key = st.secrets['USER CREDENTIALS']['API_KEY']
 
-# Getting access to NVCF credentials for inference
-NVCF_CHAT_FUNCTION_ID = st.secrets['NVCF CREDENTIALS']['NVCF_CHAT_FUNCTION_ID']
-NVCF_URL = f"https://api.nvcf.nvidia.com/v2/nvcf/pexec/functions/{NVCF_CHAT_FUNCTION_ID}"
-NVCF_API_KEY = st.secrets['NVCF CREDENTIALS']['NVCF_API_KEY']
-MODEL = "meta/llama-3.1-8b-instruct"
-os.environ['NVIDIA_API_KEY'] = NVCF_API_KEY
+# # Getting access to NVCF credentials for inference
+# NVCF_CHAT_FUNCTION_ID = st.secrets['NVCF CREDENTIALS']['NVCF_CHAT_FUNCTION_ID']
+# NVCF_URL = f"https://api.nvcf.nvidia.com/v2/nvcf/pexec/functions/{NVCF_CHAT_FUNCTION_ID}"
+# NVCF_API_KEY = st.secrets['NVCF CREDENTIALS']['NVCF_API_KEY']
+# MODEL = "meta/llama-3.1-8b-instruct"
+# os.environ['NVIDIA_API_KEY'] = NVCF_API_KEY
 
 # st.set_page_config(
 #     page_title="Demand Planner Co-Pilot",
 #     page_icon="https://www.nvidia.com/favicon.ico",
 #     initial_sidebar_state="collapsed"
 # )
+
+import os
+
+# Getting access to credentials for loading data to the database
+platform = os.getenv('PLATFORM')
+aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
+aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
+region_name = os.getenv('REGION_NAME')
+memory_location = os.getenv('BUCKET')
+number = os.getenv('NUMBER')
+
+# Getting access to user credentials to validate login
+username_credentials = os.getenv('USERNAME')
+password_credentials = os.getenv('PASSWORD')
+
+base_url = os.getenv('BASE_URL')
+api_key = os.getenv('API_KEY')
+
+NVCF_CHAT_FUNCTION_ID = os.getenv('NVCF_CHAT_FUNCTION_ID')
+NVCF_URL = f"https://api.nvcf.nvidia.com/v2/nvcf/pexec/functions/{NVCF_CHAT_FUNCTION_ID}"
+NVCF_API_KEY = os.getenv('NVCF_API_KEY')
+MODEL = "meta/llama-3.1-8b-instruct"
+os.environ['NVIDIA_API_KEY'] = NVCF_API_KEY
 
 # Initialize S3 client
 s3 = boto3.client(
